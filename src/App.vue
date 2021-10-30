eafc2706c5b8380ec4e0b23fd8379bdd6d59cc73
chuhx <chuhx1024@126.com>
2021-10-30 17:53:50 +0800

    Feat: add eslint rules

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,16 @@
 <template>
-  <div id="app">
-    <h1>你好</h1>
-    <el-row>
-        <el-button>默认按钮</el-button>
-        <el-button type="primary">主要按钮</el-button>
-        <el-button type="success">成功按钮</el-button>
-        <el-button type="info">信息按钮</el-button>
-        <el-button type="warning">警告按钮</el-button>
-        <el-button type="danger">危险按钮</el-button>
-    </el-row>
-    <router-view/>
-  </div>
+    <div id="app">
+        <h1>你好</h1>
+        <el-row>
+            <el-button>默认按钮</el-button>
+            <el-button type="primary">主要按钮</el-button>
+            <el-button type="success">成功按钮</el-button>
+            <el-button type="info">信息按钮</el-button>
+            <el-button type="warning">警告按钮</el-button>
+            <el-button type="danger">危险按钮</el-button>
+        </el-row>
+        <router-view/>
+    </div>
 </template>
 
 <style lang="scss" scoped>
